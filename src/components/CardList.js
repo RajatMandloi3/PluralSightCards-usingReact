ca7f1978eebcc2f9e5fbd92faaffecef8be86e15
rjmandloi-systango <rjmandloi@isystango.com>
2022-06-27 12:18:59 +0530

    feat:Addition of dynamic from github using name provided by user

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,13 +1,8 @@
 import Card from './Card'
-
-
-const profileImg=`https://random.imagecdn.app/500/150`
-
 export default function CardList(props) {
-
   return (
     <div>
-     {props.PERSON.map(profile=><Card img={profileImg} key={profile.id}  {...profile}/>)   }
+     {props.PERSON.map(profile=><Card  key={profile.id}  {...profile}/>)   }
     </div>
   )
 }
